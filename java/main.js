05f94978c35e929356f9f06c8f5d3b60a5eb59b7
Tristan-nl <143806032+Tristan-nl@users.noreply.github.com>
2023-10-24 14:09:33 +0200

    added an answer box

--- java/main.js
+++ java/main.js
@@ -1,2 +1,9 @@
 console.log('main loaded');
 
+const password = prompt('Insert password')
+const passWord = "Gambling"
+if (password === "Gambling") {
+    alert('Access granted')
+} else {
+    alert('Authentication failed')
+}
