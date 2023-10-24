b506237e874b8644e14293a596968e2c5543a88d
KaranJaggoe <302490410@student.rocmondriaan.nl>
2023-10-25 02:22:34 +1400

    update 24102023 1422

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 console.log('main loaded')
 console.log("het antwoord is 619")
+
+const myButton = document.querySelector('.my-button')
+myButton.addEventListener('click', myFunction);
+
+function myFunction() {
+    prompt('What is the answer?');
+} {
+    const Answer = "619";
+    if (Answer === "619") {
+        
+        alert('You won!');
+    } else {
+        
+        alert('You lost you fool');
+    }
+}
+// const result = prompt('Wat is je naam', 'JanHenk');
+// alert(`Jouw naam is ${result}`);
