fe094e3839924fa570744e2652553b4fdd4b389b
KaranJaggoe <302490410@student.rocmondriaan.nl>
2023-10-26 10:32:44 +0200

    update 26102023 1032

--- js/main.js
+++ js/main.js
@@ -5,16 +5,28 @@ const myButton = document.querySelector('.my-button')
 myButton.addEventListener('click', myFunction);
 
 function myFunction() {
-    prompt('What is the answer?');
-} {
-    const Answer = "619";
-    if (Answer === "619") {
-        
-        alert('You won!');
-    } else {
-        
-        alert('You lost you fool');
+    const answer = prompt('What is your answer?',);
+
+    if (answer === "619") {
+        alert('You win, you can go to the next room')
     }
+
+    else {
+        alert('You lost you fool, Try Again!')
+    }
+
 }
+
+
 // const result = prompt('Wat is je naam', 'JanHenk');
 // alert(`Jouw naam is ${result}`);
+
+// {
+//     prompt('What is the answer?');
+//     const Answer = "619";
+//     if (Answer === "619") {
+//         alert('You won! You can go to the next room');
+//     } else {
+//         alert('You lost you fool. Try again!');
+//     }
+// }
