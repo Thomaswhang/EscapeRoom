cb06d6e349cc1761a00b569decd9459f167aa708
Tristan-nl <143806032+Tristan-nl@users.noreply.github.com>
2023-10-26 12:12:15 +0200

    added the javascript

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.log('main loaded');
+//variables from the html
+
+const submitRebus = document.querySelector('.submit-rebus');
+const answer = "Gambling";
+const inputField = document.querySelector('.answer');
+
+//calls the HTML
+submitRebus.addEventListener('click', submitAnswer);
+
+
+//wrong answer gives an alert, correct answer sends you to the next room
+function submitAnswer() {
+    if (inputField.value == answer) {
+        alert("You are correct!");
+        window.location.assign("/Rebus puzzel/uitlegkamer.html");
+    } else {
+        alert("Wrong answer! Remember capital letters");
+    }
+} 
