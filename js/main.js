298e9b643555014ded6ab648ef779698df3aaf8e
KaranJaggoe <302490410@student.rocmondriaan.nl>
2023-10-26 13:18:23 +0200

    update 26102023 1318

--- js/main.js
+++ js/main.js
@@ -1,27 +1,59 @@
-console.log('main loaded')
-console.log("het antwoord is 619")
+// Log messages to the console
+console.log('main loaded');
+console.log("het antwoord is 619");
 
-const myButton = document.querySelector('.my-button')
-myButton.addEventListener('click', myFunction);
+ 
 
-function myFunction() {
-    const answer = prompt('What is your answer?',);
+// Event listener and function for the first button
+const myButton = document.querySelector('.my-button');
+myButton.addEventListener('click', checkAnswer);
+
+ 
+
+function checkAnswer() {
+    const answer = prompt('What is your answer?');
+
+ 
 
     if (answer === "619") {
-        alert('You win, you can go to the next room')
+        alert('You win, you can go to the next room');
+    } else {
+        alert('You lost you fool, Try Again!');
     }
-
-    else {
-        alert('You lost you fool, Try Again!')
-    }
-
 }
 
-const myButtonTwo = document.querySelector('.my-button-two')
-myButtonTwo.addEventListener('click', myFunction);
-function myFunction() {
-    alert('Do not look on F12')
-}
+ 
+
+// Event listener and function for the second button
+const myButtonTwo = document.querySelector('.my-button-two');
+myButtonTwo.addEventListener('click', function() {
+    alert('Do not look on F12');
+});
+
+// console.log('main loaded')
+// console.log("het antwoord is 619")
+
+// const myButton = document.querySelector('.my-button')
+// myButton.addEventListener('click', myFunction);
+
+// function myFunction() {
+//     const answer = prompt('What is your answer?');
+
+//     if (answer === "619") {
+//         alert('You win, you can go to the next room')
+//     }
+
+//     else {
+//         alert('You lost you fool, Try Again!')
+//     }
+
+// }
+
+// const myButtonTwo = document.querySelector('.my-button-two')
+// myButtonTwo.addEventListener('click', myFunction);
+// function myFunction() {
+//     alert('Do not look on F12')
+// }
 
 
 
