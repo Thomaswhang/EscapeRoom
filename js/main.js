653d72a2c1138f0f50b6f1668cc09a0eb9566e01
KaranJaggoe <302490410@student.rocmondriaan.nl>
2023-10-26 12:33:01 +0200

    update 26102023 1232

--- js/main.js
+++ js/main.js
@@ -17,6 +17,27 @@ function myFunction() {
 
 }
 
+const myButtonTwo = document.querySelector('.my-button-two')
+myButtonTwo.addEventListener('click', myFunction);
+function myFunction() {
+    alert('Do not look on F12')
+}
+
+
+
+
+
+// function myFunction() {
+//     if (answer === "619") {
+//         alert('You win, you can go to the next room')
+//     }
+
+//     else {
+//         alert('You lost you fool, Try Again!')
+//     }
+
+// }
+
 
 // const result = prompt('Wat is je naam', 'JanHenk');
 // alert(`Jouw naam is ${result}`);
