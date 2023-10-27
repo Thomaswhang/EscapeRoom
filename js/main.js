d5c5a28d7f769a4186b57f3d2d47759d428daf7f
Thomaswhang <143806363+Thomaswhang@users.noreply.github.com>
2023-10-27 10:02:37 +0200

    Added back chess game. Error found

--- js/main.js
+++ js/main.js
@@ -19,4 +19,28 @@ function submitAnswer1() {
     }
 } 
 
+console.log('Main Loaded')
 
+const button = document.querySelector('.my-button');
+button.addEventListener('click', function () {
+    const myDiv = document.querySelector('.my-div');
+    myDiv.textContent = 'Answer this riddle: Half a twin and a quarter of four make me, but I am not one.'
+    console.log(button);
+});
+
+const submitRebus = document.querySelector('.submit-button');
+const answer = "two";
+const inputField = document.querySelector('.answer');
+ 
+//calls the HTML
+submitRebus.addEventListener('click', submitAnswer1);
+ 
+//wrong answer gives an alert, correct answer sends you to the next room
+function submitAnswer1() {
+    if (inputField.value == answer) {
+        alert("You are correct! Press 'Ok' to go to the next room.");
+        window.location.assign("/html/Kamer6Einde.html");
+    } else {
+        alert("Wrong answer!");
+    }
+}
