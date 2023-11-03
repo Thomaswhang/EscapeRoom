c7cdffd6ff03bc3660b9a27188a2517472e6ab6f
Tristan-nl <143806032+Tristan-nl@users.noreply.github.com>
2023-11-03 09:10:07 +0100

    changed the buttons, they work better but not fully

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 console.log('main loaded');
 //variables from the html
 
-const submitRebus1 = document.querySelector('.submit-rebus');
+const submitRebus = document.querySelector('.submit-rebus');
 const answer1 = "Gambling";
 const inputField1 = document.querySelector('.answer');
 
 //calls the HTML
-submitRebus1.addEventListener('click', submitAnswer1);
+submitRebus.addEventListener('click', submitAnswer1);
 
 
 //wrong answer gives an alert, correct answer sends you to the next room
@@ -18,7 +18,7 @@ function submitAnswer1() {
     } else {
         alert("Wrong answer! Remember capital letters");
     }
-} 
+}
 
 /*
 console.log('Main Loaded')
@@ -50,85 +50,25 @@ function submitAnswer1() {
 */
 //Maria
 
+console.log('main loaded');
+//variables from the html
+
+const submitMath = document.querySelector('.submit-math');
+const answer2 = "619";
+const inputField2 = document.querySelector('.answer1');
+
+//calls the HTML
+submitMath1.addEventListener('click', submitAnswer1);
 
 
-Karan
-
-console.log("The answer is surely not 619:)");
-
-const myButton2 = document.querySelector('.my-button');
-myButton.addEventListener('click', checkAnswer);
-
- 
-
-function checkAnswer() {
-    const answer = prompt('What is your answer?');
-
- 
-
-    if (answer === "619") {
-        alert('You win, you can go to the next room');
+//wrong answer gives an alert, correct answer sends you to the next room
+function submitAnswer1() {
+    if (inputField1.value == answer2) {
+        alert("You are correct!");
+        window.location.assign("Kamer5Speluitleg.html");
     } else {
-        alert('You lost you fool, Try Again!');
+        alert("Wrong answer! Remember capital letters");
     }
 }
 
-const myButtonTwo = document.querySelector('.my-button-two');
-myButtonTwo.addEventListener('click', function() {
-    alert('Do not look on F12');
-});
-
-console.log('main loaded')
- console.log("het antwoord is 619")
-
-const myButton = document.querySelector('.my-button')
- myButton.addEventListener('click', myFunction);
-
- function myFunction() {
-     const answer = prompt('What is your answer?');
-
-    if (answer === "619") {
-         alert('You win, you can go to the next room')
-     }
-
-     else {
-         alert('You lost you fool, Try Again!')
-     }
-
- }
-
- const myButton3 = document.querySelector('.my-button-two')
- myButtonTwo.addEventListener('click', myFunction);
- function myFunction() {
-     alert('Do not look on F12')
- }
-
-
-
-
-
-function myFunction() {
-    if (answer === "619") {
-       alert('You win, you can go to the next room')
-    }
-
-   else {
-       alert('You lost you fool, Try Again!')
-    }
-
- }
-
-
-// const result = prompt('Wat is je naam', 'JanHenk');
-// alert(`Jouw naam is ${result}`);
-
- {
-     prompt('What is the answer?');
-     const Answer = "619";
-     if (Answer === "619") {
-      alert('You won! You can go to the next room');
-     } else {
-     alert('You lost you fool. Try again!');
-   }
-}
-
+console.log("answer is 619")
